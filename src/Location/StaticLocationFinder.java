c183515a0dd31fe604a2d65b1f90df8bc7fb4181
MartijnVerleij <martijn.verkley@gmail.com>
2014-04-03 09:54:48 +0200

    Initial triangulization

--- src/Location/StaticLocationFinder.java
+++ src/Location/StaticLocationFinder.java
@@ -1,14 +1,29 @@
 package Location;
 
-import Utils.MacRssiPair;
-import Utils.Position;
+import Utils.*;
 
 public class StaticLocationFinder implements LocationFinder {
 
 	@Override
 	public Position locate(MacRssiPair[] data) {
-		// TODO Auto-generated method stub
-		return null;
+		return triangulate(data);
+	}
+	
+	
+	public Position triangulate(MacRssiPair[] data) {
+		Position[] positions = new Position[data.length];
+		double x = 0;
+		double y = 0;
+		
+		for (int i = 0; i < data.length; i++) {
+			positions[i] = Utils.getKnownLocations().get(data[i].getMac());
+			x += positions[i].getX();
+			y += positions[i].getY();
+		}
+		x = x / positions.length;
+		y = y / positions.length;
+		
+		return new Position(x, y);
 	}
 
 }
